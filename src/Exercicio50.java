692eef1edc6aa914bb3ffa10d47ae236bf20a8f9
Hanna Tuburnine <hannatbr@gmail.com>
2022-12-08 18:48:00 -0500

    ExercicioLooping

---
+++ src/Exercicio50.java
@@ -0,0 +1,12 @@
+public class Exercicio50 {
+
+    public static void main(String[] args) {
+
+        int x = 2;
+
+        do {
+            System.out.println(x);
+            x = x + 2;
+        } while (x <= 48);
+    }
+}
